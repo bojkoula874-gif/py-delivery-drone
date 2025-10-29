936cf575dc5ed760d2ed69eccf79ee0f61bb8bed
Zanko Yaroslav <arikzanko9@gmail.com>
2025-10-29 10:07:39 +0200

    task

--- app/main.py
+++ app/main.py
@@ -63,5 +63,5 @@ class DeliveryDrone(FlyingRobot):
         if self.current_load is None and self.max_load_weight > cargo.weight:
             self.current_load = cargo
 
-    def unhook_load(self, cargo: Cargo) -> None:
+    def unhook_load(self) -> None:
         self.current_load = None
