a3cff9d8cdc0e37397e9cfa22826cb2df44a7e8b
Zanko Yaroslav <arikzanko9@gmail.com>
2025-10-30 08:44:42 +0200

    task

--- app/main.py
+++ app/main.py
@@ -14,6 +14,8 @@ class BaseRobot:
         self.weight = weight
         if coords is None:
             self.coords = [0, 0]
+        else:
+            self.coords = coords
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -62,7 +64,8 @@ class DeliveryDrone(FlyingRobot):
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
+        if current_load is not None:
+            self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and self.max_load_weight >= cargo.weight:
